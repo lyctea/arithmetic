f96c38e4a6758241faf62803e47d943ad58fa3fd
lyctea <396136337@qq.com>
2018-11-20 14:57:36 +0800

    <A> 错误处理

---
+++ nodejs实战/第六章connect/错误处理.js
@@ -0,0 +1,15 @@
+function errorHandler () {
+  var env = process.env.NODE_ENV || 'development';
+  return function (err, req, res, next) {
+    res.statusCode = 500;
+    switch (env) {
+      case 'development':
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(err));
+        break;
+      default:
+        res.end('Server error');
+    }
+  };
+}
+
