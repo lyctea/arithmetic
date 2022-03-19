3e1d22f1733757683522b53115894a65a5809e23
lyctea <396136337@qq.com>
2022-03-20 01:04:57 +0800

    feat:

--- 面试题/2022年03月/深入ts/手写Promise/Promise.ts
+++ 面试题/2022年03月/深入ts/手写Promise/Promise.ts
@@ -2,7 +2,7 @@
  * @Author: Jupiter
  * @Date: 2022-03-19 23:29:58
  * @LastEditors: Jupiter
- * @LastEditTime: 2022-03-19 23:46:24
+ * @LastEditTime: 2022-03-20 01:04:01
  * @Description: 文件描述
  * @FilePath: /arithmetic/面试题/2022年03月/深入ts/手写Promise/Promise.ts
  */
@@ -11,17 +11,43 @@ import { ResolveType, RejectType, Executor } from "./actionType";
 class Promise<T = any> {
   public resolve: ResolveType;
   public reject: RejectType;
+  public status: string;
+  public resolve_executor_value: any;
+  public reject_executor_value: any;
 
   constructor(executor: Executor) {
-    this.resolve = (value: any): any => {};
-    this.reject = (value: any): any => {};
+    this.status = "padding"; // 起始等待状态
+    this.resolve = (value: any): any => {
+      if (this.status === "padding") {
+        this.status = "success";
+        value[10] = 100;
+        this.resolve_executor_value = value;
+      }
+    };
+    this.reject = (value: any): any => {
+      if (this.status === "padding") {
+        this.status = "fail";
+        this.reject_executor_value = value;
+      }
+    };
 
-    executor(this.resolve, this.reject);
+    try {
+      executor(this.resolve, this.reject);
+    } catch (error) {
+      this.status = "padding";
+      this.reject(error.toString());
+      throw new Error("程序终止...");
+    }
+  }
+
+  then(resolveInThen: ResolveType, rejectinThen: RejectType) {
+    if (this.status === "success") {
+      resolveInThen(this.resolve_executor_value);
+    }
+    if (this.status === "fail") {
+      rejectinThen(this.reject_executor_value);
+    }
   }
 }
 
-new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(1);
-  }, 1000);
-});
+export default Promise;
---
+++ 面试题/2022年03月/深入ts/手写Promise/test.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: Jupiter
+ * @Date: 2022-03-20 00:07:33
+ * @LastEditors: Jupiter
+ * @LastEditTime: 2022-03-20 00:12:28
+ * @Description: 文件描述
+ * @FilePath: /arithmetic/面试题/2022年03月/深入ts/手写Promise/test.ts
+ */
+import Promise from "./Promise";
+
+let promise = new Promise((resolve, reject) => {
+  resolve("成功");
+  //   reject("失败");
+});
+
+promise.then(
+  (res) => {
+    console.log("res", res);
+  },
+  (err) => {
+    console.log("err", err);
+  }
+);
